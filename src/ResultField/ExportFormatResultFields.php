70af0d160a069369d14c19877df43732adfd8194
Marius Bippig <marius.bippig@plentymarkets.com>
2018-05-02 12:23:36 +0200

    fixed typo

--- src/ResultField/ExportFormatResultFields.php
+++ src/ResultField/ExportFormatResultFields.php
@@ -83,7 +83,7 @@ class ExportFormatResultFields extends ResultFields
         }
 
         /** @var LanguageMutator $languageMutator */
-        $languageMutator = pluginApp(LanguageMutator::class, ['language' => [$settings->get('lang')]]);
+        $languageMutator = pluginApp(LanguageMutator::class, ['languages' => [$settings->get('lang')]]);
 
 		/** @var BarcodeMutator $barcodeMutator */
 		$barcodeMutator = pluginApp(BarcodeMutator::class);
